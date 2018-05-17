cd6fbf672f4ef3795aeec3dd525b8a830a0e9199
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2018-05-16 22:53:36 -0300

    Primeiros passos para gerar automaticamente uma collection e um document vazio caso não exista. Faltando findById para completar.

--- src/main/java/com/b2w/apistarwars/PedroApiStarWarsApplication.java
+++ src/main/java/com/b2w/apistarwars/PedroApiStarWarsApplication.java
@@ -1,12 +1,35 @@
 package com.b2w.apistarwars;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PedroApiStarWarsApplication {
+import com.b2w.apistarwars.models.Planeta;
+import com.b2w.apistarwars.repository.PlanetaRepository;
 
+@SpringBootApplication
+public class PedroApiStarWarsApplication implements CommandLineRunner {
+
+	@Autowired
+	private PlanetaRepository planetrepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PedroApiStarWarsApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		if(planetrepo.count() == 0) {
+			planetrepo.save(new Planeta("Teste","Teste","Teste", "Teste"));
+		}
+		
+		System.out.println("Encontrando planetas ou gerando uma galáxia muito, muito distante...");
+		System.out.println("-------------------------------");
+		for (Planeta planeta : planetrepo.findAll()) {
+			System.out.println(planeta);
+		}
+		
 	}
 }
